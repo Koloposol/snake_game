ab3be0c74d0cf24815cbb4946ec801dd193e1d7d
Egor Lomakin <egor_lomakin@inbox.ru>
2023-10-23 17:45:48 +0400

    create game window

--- snake.py
+++ snake.py
@@ -2,10 +2,20 @@ import pygame as pg
 from random import randrange
 
 #Константы
-WINDOW = 1000
+WINDOW = 800
 FPS = 60
 
 #Инициализация объектов
 game_screen = pg.display.set_mode([WINDOW] * 2)
 clock = pg.time.Clock()
 
+#Главный цикл игры
+while True:
+    #Частота обновления экрана
+    clock.tick(FPS)
+    #Цикл обработки событий
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            exit()
+        game_screen.fill((155, 188, 15))
+        pg.display.flip()
