1128ed623dd8e24c0b91bf71890126882d586caf
Egor Lomakin <egor_lomakin@inbox.ru>
2023-10-23 17:24:54 +0400

    add cons and init objects

--- snake.py
+++ snake.py
@@ -1,3 +1,11 @@
 import pygame as pg
 from random import randrange
 
+#Константы
+WINDOW = 1000
+FPS = 60
+
+#Инициализация объектов
+game_screen = pg.display.set_mode([WINDOW] * 2)
+clock = pg.time.Clock()
+
